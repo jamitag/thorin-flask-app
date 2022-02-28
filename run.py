f0f0c39a25c150228b359629e0c76795688594ce
jamitag <jamgra12@gmail.com>
2022-02-28 14:05:47 +0000

    Link clean blog template

--- run.py
+++ run.py
@@ -20,8 +20,15 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
+© 2022 GitHub, Inc.
+Terms
